624962ed12a0f656caaeb1459234d4ef23e12f3a
Sugar <287276293@qq.com>
2024-05-14 17:08:38 +0800

    feat: add steam vr plugins

---
+++ UnityVR/Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class ItemSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Test()
+    {
+        GetComponent<VRTK_ControllerEvents>().TouchpadPressed += ItemSpawn_TouchpadPressed;
+        GetComponent<VRTK_ControllerEvents>().TouchpadReleased += ItemSpawn_TouchpadReleased;
+    }
+
+    private void ItemSpawn_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ItemSpawn_TouchpadReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
